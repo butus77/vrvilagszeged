b37ec4862a7fd95d3edd4e59ca81849e4ce93cc1
Betty <gob0504@gmail.com>
2025-04-08 11:58:42 +0200

    Elkeszult a fejlec es a hosszekcio mobilnezete

---
+++ app/HeroSection.js
@@ -0,0 +1,27 @@
+export default function HeroSection() {
+    return (
+      <section className="flex flex-col items-center text-center px-4 py-8 text-white space-y-6">
+        <h2 className="text-3xl font-bold leading-tight">
+          Lépj be a 
+        </h2>
+        <h2 className="text-3xl font-bold leading-tight text-[#67FFF2]">
+          Hyper-Realitás
+        </h2>
+        <h2 className="text-3xl font-bold leading-tight">
+         világába
+        </h2>
+        {/* Helykitöltő kép – később cserélheted */}
+        <img src="/images/Group 3.svg" alt="VR élmény" className="w-full max-w-xs rounded-lg" />
+
+  
+        <p className="text-lg font-medium">
+          A szabadmozgásos VR élmény
+        </p>
+  
+        <p className="text-sm text-[#67FFF2]">
+        Ahol nincsenek kábelek, nincsenek határok - csak Te, a valósághű fizikai interakciók, és egy teljesen új világ
+        </p>
+      </section>
+    );
+  }
+  
--- app/page.js
+++ app/page.js
@@ -1,21 +1,12 @@
 import Header from "./Header";
-
+import HeroSection from "./HeroSection";
 
 export default function Home() {
   return (
     <section className="bg-[#0e0e0f] min-h-screen flex flex-col items-center justify-between p-4 ">
       {/* Fejlec */}
       <Header />
-      {/* Tovabbi tartalom*/}
-      <div>
-        <h2 className="text-white">Lépj be a Hyper-Realitás világába</h2>
-
-        <p className="text-white">A szabadmozgásos VR élmény</p>
-        <p className="text-white">
-          Ahol nincsenek kábelek, nincsenek határok - csak Te, a valósághű
-          fizikai interakciók és egy teljesen új világ
-        </p>
-      </div>
+      <HeroSection />
     </section>
   );
 }
