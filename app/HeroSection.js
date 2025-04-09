d17308df2bd05d8a6ccf92ebab7f82ef2f5064bc
Betty <gob0504@gmail.com>
2025-04-09 11:38:11 +0200

    harmadik valtozata a kezdoszoveg eligazitasanak

--- app/HeroSection.js
+++ app/HeroSection.js
@@ -1,11 +1,13 @@
 export default function HeroSection() {
   return (
     <section className="flex flex-col items-center text-center px-4 py-8 text-white space-y-6">
-      <h2 className="text-center text-2xl sm:text-3xl font-semibold leading-tight">
-        <span className="inline">Lépj be a </span>
-        <span className="inline text-[#67FFF2]">Hyper-Realitás </span>
-        <span className="inline">világába</span>
-      </h2>
+      <div className="flex items-center justify-center">
+        <div className="text-center text-2xl sm:text-3xl font-semibold leading-tight">
+          <h2>Lépj be a</h2>
+          <h2 className="text-[#67FFF2]">Hyper-Realitás</h2>
+          <h2>világába</h2>
+        </div>
+      </div>
 
       {/* Helykitöltő kép – később cserélheted */}
       <img
