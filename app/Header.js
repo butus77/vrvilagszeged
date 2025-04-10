cc22964e21ca3eb095c2496357a55b2a8a75bf48
Betty <gob0504@gmail.com>
2025-04-10 13:09:25 +0200

    fejlec modositas

--- app/Header.js
+++ app/Header.js
@@ -3,18 +3,24 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="w-full bg-[#0E0E0F] text-white px-4 py-4  flex items-center justify-between">
-      <img
-        src="/images/VRVilág-fekvő-v 2.svg"
-        alt="VR Világ logó"
-        className="h-10 sm:h-12 md:h-14 object-contain"
-      />
-      <Link
-        href="/foglalas"
-        className="bg-[#43AFAF] text-white px-4 py-2 rounded-lg text-sm sm:text-base hover:bg-[#36a0a0] transition"
-      >
-        Foglalás
-      </Link>
+    <header className="fixed top-0 left-0 w-full bg-[#0E0E0F] text-white px-4 py-4 z-50">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logó */}
+        <Image
+          src="/images/VRVilág-fekvő-v 2.svg"
+          alt="VR Világ logó"
+          width={160}
+          height={40}
+        />
+
+        {/* Foglalás gomb */}
+        <Link
+          href="/foglalas"
+          className="bg-[#43AFAF] text-white px-4 py-2 rounded-xl text-sm font-semibold"
+        >
+          Foglalás
+        </Link>
+      </div>
     </header>
   );
 };
--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ import PostSection from "./PostSection";
 
 export default function Home() {
   return (
-    <section className="relative w-full bg-[#EAF4EC] px-4 py-12 text-center overflow-hidden">
+   
+
+    <>
       {/* Fejlec */}
       <Header />
       <HeroSection />
       <PostSection />
-    </section>
+      </>
   );
 }
