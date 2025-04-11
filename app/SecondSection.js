1d248673557222b76fc85f561e724a4763308c49
Betty <gob0504@gmail.com>
2025-04-11 12:38:47 +0200

    meg nem az igazi

--- app/SecondSection.js
+++ app/SecondSection.js
@@ -1,9 +1,9 @@
 export default function SecondSection() {
   return (
     <>
-      <section className="relative bg-[#EAF4EC] px-4 py-12 text-center flex flex-col items-center">
-        {/* Címsor */}
-        <h2 className="pt-28 text-2xl sm:text-3xl md:text-4xl font-bold text-[#43AFAF] z-10 relative">
+      <section className="relative bg-[#EAF4EC] px-4 py-12 text-center flex flex-col items-center pt-24  space-y-10">
+            {/* Címsor */}
+        <h2 className="pt-28 text-2xl sm:text-3xl md:text-4xl font-bold text-[#43AFAF] z-10 text-center relative">
           A valóság már nem kötelező!
         </h2>
 
