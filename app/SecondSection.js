0b6c74d4af137da77c05bee9e9bd1d826d2d05a6
Betty <gob0504@gmail.com>
2025-04-11 11:13:41 +0200

    secondsection ujabb valtoztatasok

--- app/SecondSection.js
+++ app/SecondSection.js
@@ -1,12 +1,7 @@
 export default function PostSection() {
   return (
     <>
-      <img
-        src="/images/Vector 1.svg"
-        alt="Dekoratív elválasztó"
-        className="absolute bottom-0  w-full z-0"
-      />
-      <section className="relative bg-[#EAF4EC] px-4 py-12 text-center">
+      <section className="relative bg-[#EAF4EC] px-4 py-12 text-center overflow-visible">
         <div
           className="absolute w-[1001px] h-[1781px] top-[-2px] left-[-1px] bg-cover bg-no-repeat bg-center"
           style={{ backgroundImage: "url('/images/bg-dekor.png')" }}
@@ -25,7 +20,16 @@ export default function PostSection() {
             className="w-[228px] h-[228px] object-contain z-10"
           />
         </div>
-        <h4 className="pt-28  sm:text-2xl md:text-3xl font-bold text-[#43AFAF] z-10 relative">Csapatos vagy egyéni kalandok, ahol minden lépés számít</h4>
+        <h4 className="pt-28  sm:text-2xl md:text-3xl font-bold text-[#43AFAF] z-10 relative">
+          Csapatos vagy egyéni kalandok, ahol minden lépés számít
+        </h4>
+        <div
+          className="absolute -bottom-12 bg-[#EAF4EC] w-full h-44 z-10"
+          style={{
+            clipPath:
+              "polygon(50% 69%, 100% 68%, 100% 75%, 50% 100%, 0% 75%, 0 69%)",
+          }}
+        ></div>
       </section>
     </>
   );
