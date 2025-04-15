dcdd936f01d06e43a6d9631f18b509a9f7d2f5e2
Betty <gob0504@gmail.com>
2025-04-15 19:31:10 +0200

    responzivitas es uj kep

--- app/Header.js
+++ app/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-[#0E0E0F] text-white px-4 py-4 z-50">
-      <div className="flex justify-between items-center max-w-7xl mx-auto">
+      <div className="flex justify-between items-center max-w-7xl mx-auto sm:">
         {/* Logó */}
         <Image
           src="/images/VRVilág-fekvő-v 2.svg"
@@ -13,13 +13,33 @@ const Header = () => {
           height={20}
         />
 
-        {/* Foglalás gomb */}
-        <Link
-          href="/foglalas"
-          className="bg-[#43AFAF] text-white px-4 py-2 rounded-xl text-sm font-semibold sm:text-base absolute right-4 top-4 sm:right-8"
-        >
-          Foglalás
-        </Link>
+        <div className="hidden sm:flex items-center space-x-4">
+          <Image
+            src="/images/Frame 63.svg"
+            alt="kozosegi media logok"
+            width={100}
+            height={20}
+            className="w-auto h-5"
+          />
+
+          {/* Foglalás gomb */}
+          <Link
+            href="/foglalas"
+            className="bg-[#43AFAF] text-white px-4 py-2 rounded-xl text-sm font-semibold sm:text-base right-4 top-4 sm:right-8"
+          >
+            Foglalás
+          </Link>
+        </div>
+
+        {/* Mobil gomb (kép nélkül) */}
+        <div className="flex sm:hidden">
+          <Link
+            href="/foglalas"
+            className="bg-[#43AFAF] text-white px-4 py-2 rounded-xl text-sm font-semibold"
+          >
+            Foglalás
+          </Link>
+        </div>
       </div>
     </header>
   );
--- app/SecondSection.js
+++ app/SecondSection.js
@@ -3,12 +3,12 @@ export default function SecondSection() {
     <>
       <section className="relative bg-[#EAF4EC] px-4 py-12 text-center flex flex-col items-center pt-24  space-y-10">
         {/* Címsor */}
-        <h2 className="pt-28 text-2xl sm:text-3xl md:text-4xl font-bold text-[#43AFAF] z-10 text-center relative">
+        <h2 className="pt-10 text-2xl sm:text-3xl md:text-4xl font-bold text-[#43AFAF] z-10 text-center relative">
           A valóság már nem kötelező!
         </h2>
 
         {/* Kép (haptic) */}
-        <div className="mt-12 relative z-10 w-[228px] h-[228px]">
+        <div className="relative z-10 w-[228px] h-[228px]">
           <img
             src="/images/haptic 1.svg"
             alt="Haptikus élmény"
@@ -22,9 +22,10 @@ export default function SecondSection() {
             title="Mellkasi rezgés"
           ></button>
 
-          <div className="absolute top-4 left-24 sm:left-52 max-w-[140px] break-words w-40 text-sm text-black font-bold text-left z-30">
+          <div className="absolute top-4 left-48 w-28 sm:w-40 text-sm text-black max-w-min sm:max-w-[200px] md:max-w-[210px] font-bold text-left text-wrap break-words">
             Haptikus mellényekkel érzed a találatokat, ütéseket és rezgéseket.
           </div>
+
           {/* Bal oldal közepe */}
           <button
             className="absolute bottom-12 left-2 z-20 bg-[#43AFAF] w-6 h-6 rounded-full hover:bg-[#367D7E] transition"
@@ -33,11 +34,13 @@ export default function SecondSection() {
 
           {/* Alsó jobb sarok */}
           <button
-            className="absolute bottom-7 right-14 z-20 bg-[#43AFAF] w-6 h-6 rounded-full hover:bg-[#367D7E] transition"
+            className="absolute bottom-11 right-14 z-20 bg-[#43AFAF] w-6 h-6 rounded-full hover:bg-[#367D7E] transition"
             title="Alsó rész"
           ></button>
-          <div className="absolute -bottom-10  left-36 sm:left-46 max-w-[140px] break-words
-w-40 text-sm text-orange-700 text-left font-bold z-50">
+          <div
+            className="absolute -bottom-22 sm:-bottom-6 left-36 text-black sm:left-40 max-w-min sm:max-w-[200px] md:max-w-[210px] break-words
+w-28 text-sm sm:w-40 md:w-80 text-left font-bold z-50"
+          >
             Haptikus mellényekkel érzed a találatokat, ütéseket és rezgéseket.
           </div>
         </div>
