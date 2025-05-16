1af0d108a93a0b911717a6051e01bc6c7a1bcfc3
Betty <gob0504@gmail.com>
2025-05-16 22:03:33 +0200

    pontositasok

--- app/Foglalj.js
+++ app/Foglalj.js
@@ -2,13 +2,13 @@ export default function Foglalj() {
   return (
     <>
       {/* Címsor */}
-      <h2 className="py-10 text-center text-[#EAF4EC] text-4xl sm:text-6xl md:text-7xl font-bold leading-tight">
+      <h2 className="py-2 text-center text-[#EAF4EC] text-4xl sm:text-6xl md:text-7xl font-bold leading-tight">
         Foglalj!
       </h2>
 
       {/* Lépések gridben */}
       <section className="bg-transparent px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 gap-x-8 max-w-6xl mx-auto py-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 gap-x-8 max-w-6xl mx-auto py-10">
           {/* 1. lépés */}
           <div className="flex items-start gap-3">
             <div className="border-2 border-[#43AFAF] text-[#43AFAF] font-bold px-3 py-1 text-lg">
